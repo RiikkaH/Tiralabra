928a6baa97c80f26c04e4a402b94ccc7152c56af
RiikkaH <riikka.huusari@helsinki.fi>
2013-12-30 21:59:57 +0200

    fibonaccikekoa eteenpäin, edelleen

--- KekoVertailu/src/kekovertailu/Fibonaccikeko.java
+++ KekoVertailu/src/kekovertailu/Fibonaccikeko.java
@@ -13,13 +13,13 @@ public class Fibonaccikeko {
     public Fibonaccikeko() {
         keko = null;
         min = keko;
-        solmuja=0;
+        solmuja = 0;
     }
 
     public Fibonaccikeko(Solmu s) {
         keko = s;
         min = s;
-        solmuja=1;
+        solmuja = 1;
     }
 
     /**
@@ -30,6 +30,7 @@ public class Fibonaccikeko {
     public Solmu findMin() {
         return min;
     }
+
     /**
      * Poistaa keon pienimmän alkion ja tiivistää keon.
      */
@@ -38,119 +39,140 @@ public class Fibonaccikeko {
         paivitaMin();
         yhdistaKeonPuut();
     }
+
     /**
-     * Yhdistelee keon puita niin, ettei millään kahdella puulla ole samaa astetta.
+     * Yhdistelee keon puita niin, ettei millään kahdella puulla ole samaa
+     * astetta.
      */
-    private void yhdistaKeonPuut(){
-        Solmu[] lista=new Solmu[(int)Math.log(solmuja)+1];//mikähän logaritmi pitäisi olla?
-        Solmu nyk=keko;
-        while(nyk.getSeuraava()!=null){
-            if(lista[nyk.getAste()]!=null){
-                lista[nyk.getAste()]=nyk;
-                nyk=nyk.getSeuraava();
-            }else{
-                Solmu uusi=yhdistaPuut(nyk,lista[nyk.getAste()]);
-                lista[nyk.getAste()]=null;
-                lista[uusi.getAste()]=uusi;
-                uusi.setSeuraava(nyk.getSeuraava());
-                nyk=uusi;
-                //edellisestä linkki tähän? sama juttu kuin binomikeko.merge
-                //pitäisikö sittenkin tehdä kahteen suuntaan linkitetty lista?
+    private void yhdistaKeonPuut() {
+        Solmu[] lista = new Solmu[(int) Math.log(solmuja) + 1];//mikähän logaritmi pitäisi olla?
+        Solmu nyk = keko;
+        while (nyk != null) {
+            if (lista[nyk.getAste()] == null) {
+                lista[nyk.getAste()] = nyk;
+                nyk = nyk.getSeuraava();
+            } else {
+                Solmu s=lista[nyk.getAste()];
+                if(s.getEdellinen()!=null){
+                    s.getEdellinen().setSeuraava(s.getSeuraava());
+                    s.getSeuraava().setEdellinen(s.getEdellinen());
+                }else{
+                    s.getSeuraava().setEdellinen(null);
+                }
+                Solmu ed=nyk.getEdellinen();
+                //nyt yhdistetään nyk ja s, ja laitetaan yhdistetty ed:istä 
+                //seuraavaksi. s on jo poistettu nätisti, siitä vois tehdä oman metodin.
+                //sitten uusi yhdistetty solmu laitetaan nyk:in paikalle,
+                //ja se on seuraava nyk.
             }
         }
     }
-    private Solmu yhdistaPuut(Solmu s, Solmu t){
-        if(s.getArvo()<t.getArvo()){
-            Solmu apu=s;
-            s=t;
-            t=apu;
+
+    private Solmu yhdistaPuut(Solmu s, Solmu t) {
+        if (s.getArvo() < t.getArvo()) {
+            Solmu apu = s;
+            s = t;
+            t = apu;
         }
-        lisaaLapsi(t,s);
+        lisaaLapsi(t, s);
         return t;
     }
+
     /**
      * Lisää solmulle p lapsen l
+     *
      * @param p solmu, johon lapsi lisätään
      * @param l lisättävä lapsi
      */
     //lisättävä tulee aina lapsista viimeiseksi
-    private void lisaaLapsi(Solmu p,Solmu l){
-        if(p.getAste()==0){
+    private void lisaaLapsi(Solmu p, Solmu l) {
+        if (p.getAste() == 0) {
             l.setParent(p);
             p.setLapsi(l);
-        }
-        else {
-            Solmu s=p.getLapsi();
-            while(s.getSeuraava()!=null){
-                s=s.getSeuraava();
+        } else {
+            Solmu s = p.getLapsi();
+            while (s.getSeuraava() != null) {
+                s = s.getSeuraava();
             }
             s.setSeuraava(l);
         }
-        p.setAste(p.getAste()+1);
+        p.setAste(p.getAste() + 1);
     }
-    public Solmu getKeko(){
+
+    public Solmu getKeko() {
         return keko;
     }
+
     /**
      * Päivittää min:in arvon.
      */
-    private void paivitaMin(){
-        Solmu uusimin=keko;
-        Solmu s=keko;
-        while(s.getSeuraava()!=null){
-            if(s.getArvo()<uusimin.getArvo()){
-                uusimin=s;
+    private void paivitaMin() {
+        Solmu uusimin = keko;
+        Solmu s = keko;
+        while (s.getSeuraava() != null) {
+            if (s.getArvo() < uusimin.getArvo()) {
+                uusimin = s;
             }
-            s=s.getSeuraava();
+            s = s.getSeuraava();
         }
-        min=uusimin;
+        min = uusimin;
     }
+
     /**
      * Poistaa keosta minimialkion ja lisää sen lapset kekoon.
      */
     private void poistaMinLisaaLapset() {
         Solmu uudetpuut = min.getLapsi();
+        Solmu s = keko;
+        while (s.getSeuraava() != min) {
+            s = s.getSeuraava();
+        }
+        if (s.getSeuraava().getSeuraava() != null) {
+            Solmu apu=s.getSeuraava().getSeuraava();
+            s.setSeuraava(apu);
+            apu.setEdellinen(s);
+            
+        }else{
+            s.setSeuraava(null);
+        }
         if (uudetpuut != null) {
-            uudetpuut.setParent(null);
-            Solmu s = keko;
-            while (s.getSeuraava() != min) {
+            while (s.getSeuraava() != null) {
                 s = s.getSeuraava();
             }
-            if(s.getSeuraava().getSeuraava()!=null){
-                s.setSeuraava(s.getSeuraava().getSeuraava());
-                while(s.getSeuraava()!=null){
-                    s=s.getSeuraava();
-                }
-            }
-            s.setSeuraava(uudetpuut); 
+            uudetpuut.setParent(null);//pitääkö asettaa jokaisen parent, onko jokaisella parent?
+            uudetpuut.setEdellinen(s);
+            s.setSeuraava(uudetpuut);
         }
     }
+
     /**
      * Vähentää solmun s arvon d:hen.
+     *
      * @param s solmu, jonka arvoa vähennetään
      * @param d solmun uusi arvo
      */
     public void decreaseKey(Solmu s, int d) {
-        
+
     }
 
     /**
      * Lisää kekoon uuden solmun arvolla key.
+     *
      * @param key kekoon lisättävän solmun arvo
      */
     public void insert(int key) {
         Solmu s = new Solmu(key, null, null, 0);
-        if(keko==null){
-            keko=s;
-            min=s;
-        }else{
-            if(keko.getSeuraava()!=null){
-                Solmu apu=keko.getSeuraava();
+        if (keko == null) {
+            keko = s;
+            min = s;
+        } else {
+            if (keko.getSeuraava() != null) {
+                Solmu apu = keko.getSeuraava();
                 keko.setSeuraava(s);
                 s.setSeuraava(apu);
                 apu.setEdellinen(s);
                 s.setEdellinen(keko);
-            }else{
+            } else {
                 keko.setSeuraava(s);
                 s.setEdellinen(keko);
             }
@@ -160,8 +182,10 @@ public class Fibonaccikeko {
         }
         solmuja++;
     }
+
     /**
      * Yhdistää kaksi fibonaccikekoa.
+     *
      * @param k toinen yhdistettävä keko
      */
     public void merge(Fibonaccikeko k) {
