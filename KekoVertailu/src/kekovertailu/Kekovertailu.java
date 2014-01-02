302da69d8abf0ff45bbad6de1d6666bf15514b5f
RiikkaH <riikka.huusari@helsinki.fi>
2014-01-02 21:44:06 +0200

    aikavertailu alulle

--- KekoVertailu/src/kekovertailu/Kekovertailu.java
+++ KekoVertailu/src/kekovertailu/Kekovertailu.java
@@ -8,7 +8,31 @@ package kekovertailu;
 public class Kekovertailu {
 
     public static void main(String[] args) {
+        mittaaJarjestamisenAikoja();
+    }
+    
+    public static void mittaaJarjestamisenAikoja(){
+        int[] taulukko=new int[10000];
+        for(int i=0;i<10000;i++){
+            taulukko[i]=(int)Math.random()*10000;
+        }
+        Kekojarjestaminen jarjesta=new Kekojarjestaminen(taulukko);
         
+        long alku=System.currentTimeMillis();
+        jarjesta.jarjestaBinaarikeolla();
+        System.out.println("Binäärikeko: "+(System.currentTimeMillis()-alku));
+        
+        alku=System.currentTimeMillis();
+        jarjesta.jarjestaDarykeolla(4);
+        System.out.println("d-arykeko(4): "+(System.currentTimeMillis()-alku));
+        
+        alku=System.currentTimeMillis();
+        jarjesta.jarjestaBinomikeolla();
+        System.out.println("Binomikeko: "+(System.currentTimeMillis()-alku));
+        
+        alku=System.currentTimeMillis();
+        jarjesta.jarjestaFibonaccikeolla();//ja miksi ihmeessä ei muka toimi??
+        System.out.println("Fibonaccikeko: "+(System.currentTimeMillis()-alku));
     }
     
 }
